1bdd450d770321547009561716bea62d26d13131
Joe Webb <webbdeveloping@gmail.com>
2023-04-27 00:17:22 -0600

    change default hero image

--- client/src/app/components/02-molecules/Hero/utils/slides.js
+++ client/src/app/components/02-molecules/Hero/utils/slides.js
@@ -3,7 +3,7 @@ export const slides = [
     title: "GEAR FOR MODERN FELINE WARRIORS",
     subtitle: "Stealthy Whiskers",
     image:
-      "https://assets.website-files.com/642fc428f0c0b966d5ba7a46/64305b76d2d6ae6e2a460d9c_bg-2.webp",
+      "https://assets.website-files.com/642fc428f0c0b966d5ba7a46/64305b76ca8aeb71e222e610_bg-5.webp",
     button: {
       text: "Shop Now 11",
       link: "/catalog",
@@ -13,7 +13,7 @@ export const slides = [
     title: "GEAR FOR MODERN FELINE WARRIORS",
     subtitle: "Stealthy Whiskers",
     image:
-      "https://assets.website-files.com/642fc428f0c0b966d5ba7a46/64305b76ca8aeb71e222e610_bg-5.webp",
+      "https://assets.website-files.com/642fc428f0c0b966d5ba7a46/64305b76d2d6ae6e2a460d9c_bg-2.webp",
     button: {
       text: "Shop Now2",
       link: "/catalog",
